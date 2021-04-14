6f8d91c409be41123fed9f4ad1aaa61398d3f93e
Ali <alibaniasad1999@yahoo.com>
2021-04-14 13:04:29 +0430

    remove mines and fix something from meeting with nima

--- HW/HW1/Code/Q2/Q2_a.m
+++ HW/HW1/Code/Q2/Q2_a.m
@@ -5,21 +5,3 @@ eq1 = diff(eq, x);
 % df/dy %
 eq2 = diff(eq, y);
 % Gradiant f solver %
-[sol_x, sol_y] = solve([eq1, eq2], [x, y]);
-% calculate z with subject %
-sol_z = sin(sol_x) + cos(sol_y);
-% function %
-f = sol_x^2 + sol_y^2 + sol_z^2;
-% Distance %
-r = sqrt(f);
-rx = [0, sol_x];
-ry = [0, sol_y];
-rz = [0, sol_z];
-[X, Y] = meshgrid(-1:.1:1);
-Z = sin(X) + cos(Y);
-[RX, RY] = meshgrid(-0.5787224241179413703304116508523:.1:0.5787224241179413703304116508523);
-RZ = sin(RX) + cos(RY);
-plot(rx, ry)
-hold;
-contour(X, Y, Z)
-contour(RX, RY, RZ)
