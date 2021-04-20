d47e626287142410a1727ddd96edb57f18ffa6f7
Ali <alibaniasad1999@yahoo.com>
2021-04-20 19:14:31 +0430

    add final rope

--- HW/HW1/Code/Q4/Q4.m
+++ HW/HW1/Code/Q4/Q4.m
@@ -1,4 +1,6 @@
-syms y(x)
-syms lambda
-ode = (y - lambda) * diff(y, x, 2) + ...
-    diff(y, x)^2 + diff(y, x)^4;
+syms a b lambda tf l
+eq1 = a * sinh((tf - b) / a) - l;
+eq2 = lambda + a * cosh(b / a);
+eq3 = lambda + a * cosh((tf - b) / a);
+[solve_a, solve_b, solve_lambda] = ...
+fzero([eq1, eq2, eq3], [a, b, lambda]);
