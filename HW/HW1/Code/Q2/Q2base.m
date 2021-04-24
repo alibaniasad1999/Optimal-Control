5b3a77ea114461b3ebcfa6a81d63e7c160fcf961
Ali <alibaniasad1999@yahoo.com>
2021-04-24 18:40:24 +0430

    add question 2 figure in two view

--- HW/HW1/Code/Q2/Q2base.m
+++ HW/HW1/Code/Q2/Q2base.m
@@ -1,3 +1,17 @@
 [X,Y] = meshgrid(linspace(-4, 4));
 Z = sin(X) + cos(Y);
-mesh(X, Y, Z);
+Q2_b;
+r =sqrt(sol_x^2 + sol_y^2 + sol_z^2);
+r = double(r);
+[xs, ys, zs] = sphere();
+xs = r * xs;
+ys = r * ys;
+zs = r * zs;
+figure1 = figure('Name','Mesh Grid','NumberTitle','off');
+plot3(X, Y, Z);
+hold;
+xlabel('x')
+ylabel('y')
+zlabel('z')
+mesh(xs, ys, zs);
+print(figure1, 'plotwithSphere.png','-dpng','-r300');
