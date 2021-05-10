f947f74b9dfe848032edacffb2c9126ce341fb5f
Ali <alibaniasad1999@yahoo.com>
2021-05-10 15:51:45 +0430

    add code question and ode equation

---
+++ HW/HW2/Code/Q3/Q3_a.m
@@ -0,0 +1,11 @@
+syms x1(t) x2(t) p1(t) p2(t)
+ode1 = diff(x1) == x2;
+ode2 = diff(x2) == -x1 - 0.1 * x2 - 0.5 * p2;
+ode3 = diff(p1) == -2 * x1 + p2;
+ode4 = diff(p2) == -2 * x2 - p1 + 0.1 * p2;
+odes = [ode1; ode2; ode3; ode4];
+% cond1 = x1(0) == 1;
+% cond2 = x2(0) == 1;
+% conds = [cond1; cond2];
+% [x1Sol(t), x2Sol(t), p1Sol(t), p2Sol(t)] = dsolve(odes,conds);
+[x1Sol(t), x2Sol(t), p1Sol(t), p2Sol(t)] = dsolve(odes);
