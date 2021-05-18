d0f926c4ae3bf232a44524ad193bd5bc7ca9a7e8
Ali <alibaniasad1999@yahoo.com>
2021-05-19 00:34:58 +0430

    add all king of figure in code

--- HW/HW2/Code/Q3/Q3_c.m
+++ HW/HW2/Code/Q3/Q3_c.m
@@ -36,10 +36,11 @@ for i = 1:length(tf_arr)
     figure1u = figure('Name',figure_name, 'NumberTitle','off');
     %%% calculate u(t) %%%
     ue = zeros(1);
-    for j = 1:length(t_K)
-        ue(j) = -R_inv*B'*reshape(K_arr(j, :), n, n)*x(j,:)';
+    K_t	= interp1(t_K, K_arr, t);
+    for j = 1:length(t)
+        ue(j) = -R_inv*B'*reshape(K_t(j, :), n, n)*x(j,:)';
     end
-    plot(t_K, ue);
+    plot(t, ue);
     xlabel('time');
     ylabel('u');
     figure_save_name = append('figures/u', num2str(tf), '.png');
@@ -56,7 +57,7 @@ for i = 1:length(tf_arr)
     close;
 end
 %%%% in one subplot %%%%
-%%% k(t) %%%%
+%%% K(t) %%%%
 for i = 1:length(tf_arr)
     %%% initial final time %%%
     tf = tf_arr(i);
@@ -66,7 +67,7 @@ for i = 1:length(tf_arr)
     [t, x] = ode45(@diff_eq_states, [0, tf], x0);
     subplot(2,2,i);
     plot(t_K, K_arr);
-    figure_name = strcat('System response tf =  ', num2str(tf));
+    figure_name = strcat('K(t) tf =  ', num2str(tf));
     title(figure_name);
     xlabel('time');
     ylabel('K');
@@ -84,11 +85,12 @@ for i = 1:length(tf_arr)
     [t, x] = ode45(@diff_eq_states, [0, tf], x0);
     subplot(2,2,i);
     ue = zeros(1);
-    for j = 1:length(t_K)
-        ue(j) = -R_inv*B'*reshape(K_arr(j, :), n, n)*x(j,:)';
+    K_t	= interp1(t_K, K_arr, t);
+    for j = 1:length(t)
+        ue(j) = -R_inv*B'*reshape(K_t(j, :), n, n)*x(j,:)';
     end
-    plot(t_K, ue);
-    figure_name = strcat('System response tf =  ', num2str(tf));
+    plot(t, ue);
+    figure_name = strcat('u(t) tf =  ', num2str(tf));
     title(figure_name);
     xlabel('time');
     ylabel('u');
@@ -123,16 +125,80 @@ for i = 0:2
     [t_K, K_arr] = ode45(@diff_eq_Riccati, [tf, 0], K0);
     %%% solve System equation %%%
     [t, x] = ode45(@diff_eq_states, [0, tf], x0);
+    %%% K %%%
     figure_name = append('System response H = ', num2str(10^i), 'I');
+    figure1K = figure('Name',figure_name, 'NumberTitle','off');
+    plot(t_K, K_arr);
+    xlabel('time');
+    ylabel('K');
+    legend('$K_1$', '$K_2$', '$K_3$', '$K_4$', 'Interpreter','latex');
+    figure_save_name = append('figures/KH', num2str(10^i), '.png');
+    print(figure1K, figure_save_name,'-dpng','-r400');
+    close;
+    %%% u %%%
+    figure1u = figure('Name',figure_name, 'NumberTitle','off');
+    %%% calculate u(t) %%%
+    ue = zeros(1);
+    K_t	= interp1(t_K, K_arr, t);
+    for j = 1:length(t)
+        ue(j) = -R_inv*B'*reshape(K_t(j, :), n, n)*x(j,:)';
+    end
+    plot(t, ue);
+    xlabel('time');
+    ylabel('u');
+    figure_save_name = append('figures/uH', num2str(10^i), '.png');
+    print(figure1u, figure_save_name,'-dpng','-r400');
+    close;
+    %%% states %%%
     figure1 = figure('Name',figure_name, 'NumberTitle','off');
     plot(t, x)
     xlabel('time');
     ylabel('x');
-    legend('$x_1$', '$x_2$','Interpreter','latex');
-    figure_save_name = append('figures/H', num2str(10^i), '.png');
+    legend('$x_1$', '$x_2$', 'Interpreter','latex');
+    figure_save_name = append('figures/xH', num2str(10^i), '.png');
     print(figure1, figure_save_name,'-dpng','-r400');
+    close;
 end
 %%%% in one subplot %%%%
+%%% K(t) %%%
+for i = 0:2
+    H = 10^i * eye(2);
+    K0 = H;
+    %%% solve diffrential ricaati %%%
+    [t_K, K_arr] = ode45(@diff_eq_Riccati, [tf, 0], K0);
+    subplot(2,2,i+1);
+    plot(t_K, K_arr)
+    figure_name = append('K(t) H = ', num2str(10^i), 'I');
+    title(figure_name);
+    xlabel('time');
+    ylabel('K');
+    legend('$K_1$', '$K_2$', '$K_3$', '$K_4$', 'Interpreter','latex');
+end
+print('figures/SubplotQ3_cKII.png','-dpng','-r600');
+close;
+%%% u(t) %%%
+for i = 0:2
+    H = 10^i * eye(2);
+    K0 = H;
+    %%% solve diffrential ricaati %%%
+    [t_K, K_arr] = ode45(@diff_eq_Riccati, [tf, 0], K0);
+    %%% solve System equation %%%
+    [t, x] = ode45(@diff_eq_states, [0, tf], x0);
+    subplot(2,2,i+1);
+    ue = zeros(1);
+    K_t	= interp1(t_K, K_arr, t);
+    for j = 1:length(t)
+        ue(j) = -R_inv*B'*reshape(K_t(j, :), n, n)*x(j,:)';
+    end
+    plot(t, ue);
+    figure_name = append('u(t) H = ', num2str(10^i), 'I');
+    title(figure_name);
+    xlabel('time');
+    ylabel('u');
+end
+print('figures/SubplotQ3_cuII.png','-dpng','-r600');
+close;
+%%% states %%%%
 for i = 0:2
     H = 10^i * eye(2);
     K0 = H;
@@ -149,6 +215,7 @@ for i = 0:2
     legend('$x_1$', '$x_2$','Interpreter','latex');
 end
 print('figures/SubplotQ3_cII.png','-dpng','-r600');
+close;
 %% Functions %%
 %%% Riccati %%%
 function d = diff_eq_Riccati(~,k)
