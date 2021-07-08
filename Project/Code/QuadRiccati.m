04bfc8d9ece8e817fb12cd29f97c0d770bb98378
Ali <alibaniasad1999@yahoo.com>
2021-07-09 00:28:12 +0430

    add quadcopter LQR solver

--- Project/Code/QuadRiccati.m
+++ Project/Code/QuadRiccati.m
@@ -1,19 +1,50 @@
-%%% Quad in zero LQR solver %%%
-QuadSpaceState;
-% Weight Matrix %
-% syms Q11 Q22 Q33 Q44 Q55 Q66
-% Q = [Q11 0   0   0   0   0   ;
-%      0   Q22 0   0   0   0   ;
-%      0   0   Q33 0   0   0   ;
-%      0   0   0   Q44 0   0   ;
-%      0   0   0   0   Q55 0   ;
-%      0   0   0   0   0   Q66];
-% syms R11 R22 R33 R44
-% R = [R11 0   0   0   ;
-%      0   R22 0   0   ;
-%      0   0   R33 0   ;
-%      0   0   0   R44];
-% Riccati Solver %
-Q = eye(6);
-R = eye(4);
-% algebraic_riccati = K * A + A' * K - K * B * R^-1 * B' * K + Q
+global A B Q R n R_inv
+QuadConstants;
+A = [0  0  0 1 0 0  ;
+     0  0  0 0 1 0  ;
+     0  0  0 0 0 1  ;
+     A1 0  0 0 0 0  ;
+     0  B1 0 0 0 0  ;
+     0  0  0 0 0 0] ;
+B = [0   0   0   0  ;
+     0   0   0   0  ;
+     0   0   0   0  ;
+     0   A3  0  -A3 ;
+     B3  0  -B3  0  ;
+     C2 -C2  C2  C2];
+Q	= 1000*eye(6);
+R	= 1;
+R_inv = 1;
+H	= 10*eye(6);
+tf	= 100;
+
+K0	= H;
+n	= 6;
+k0	= reshape(K0,n^2,1);
+global t_K K_arr
+[t_K,K_arr] = ode45(@diff_eq_Riccati,[tf,0],k0);
+x0	= ones(6, 1)*0.1;
+[t,x] = ode45(@diff_eq_states,[0,tf],x0);
+% x(:, 1:3) = wrapToPi(x(:, 1:3));
+plot(t, x)
+figure(101)
+plot(t, x)
+legend('$\phi$', '$\theta$', '$\psi$','$p$','$q$','$r$',...
+    'interpreter', 'latex') 
+%% Functions %%
+function d = diff_eq_Riccati(~,k)
+global A B Q R_inv n
+K	= reshape(k,n,n);
+Kdot	= -K*A - A'*K - Q + K*B*R_inv*B'*K;
+d	= reshape(Kdot,n^2,1);
+end
+
+function d = diff_eq_states(t,x)
+global A B R_inv n
+global t_K K_arr
+K_t	= interp1(t_K, K_arr, t);
+K	= reshape(K_t,n,n);
+u	= -R_inv*B'*K*x;
+d	= A*x + B*u;
+end
+
--- Project/Code/Quad_ode45.m
+++ Project/Code/Quad_ode45.m
@@ -1,15 +1,29 @@
 %%%%%%%%%%%%%%%%%%%%%% Quadcopter ODE45 %%%%%%%%%%%%%%%%%%%%%%
-global A B
+QuadConstants;
+global A B k R
 A = [0  0  0 1 0 0  ;
      0  0  0 0 1 0  ;
      0  0  0 0 0 1  ;
      A1 0  0 0 0 0  ;
      0  B1 0 0 0 0  ;
      0  0  0 0 0 0] ;
-[t, x] = ode45(@diff_equ, [0, 10], zeros(6, 1));
+B = [0   0   0   0  ;
+     0   0   0   0  ;
+     0   0   0   0  ;
+     0   A3  0  -A3 ;
+     B3  0  -B3  0  ;
+     C2 -C2  C2  C2];
+Q = 10 * eye(6);
+R = eye(4);
+[k, ~, ~] = icare(A, B, Q, R);
+[t, x] = ode45(@diff_equ, [0, 10], [1; 1 ; 1; zeros(3, 1)+1]);
+x(:, 1:3) = wrapToPi(x(:, 1:3));
 plot(t, x)
+legend('$\phi$', '$\theta$', '$\psi$','$p$','$q$','$r$',...
+    'interpreter', 'latex') 
 function d = diff_equ(~, X)
 x = X(1:6);
-global A
-d = A*x;
+x(1:3) = wrapToPi(x(1:3));
+global A B k R
+d = (A - B * R^-1 * B' * k) * x;
 end
