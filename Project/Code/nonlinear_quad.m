3839c56fbbc5f8b4c0585305311ae0c0ca0a6a74
Ali <alibaniasad1999@yahoo.com>
2021-07-10 14:32:15 +0430

    add nonlinear quad LQR solver

--- Project/Code/nonlinear_quad.m
+++ Project/Code/nonlinear_quad.m
@@ -4,16 +4,21 @@ global R Q
 Q = 10 * eye(6);
 R = eye(4);
 x0	= [0 ;
-       1 ;
-       0 ;
-       0 ;
-       0 ;
-       0];
-[t, x] = ode45(@diff_equ, [0, 1], x0);
+    1 ;
+    0 ;
+    0 ;
+    0 ;
+    0];
+tic
+[t, x] = ode45(@diff_equ, [0, 10], x0);
 x(:, 1:3) = wrapToPi(x(:, 1:3));
+toc
 plot(t, x)
+xlabel('$Time_{\sec}$', 'interpreter', 'latex');
+ylabel('$\vec{X}$', 'interpreter', 'latex');
 legend('$\phi$', '$\theta$', '$\psi$','$p$','$q$','$r$',...
-    'interpreter', 'latex') 
+    'interpreter', 'latex')
+print('../Figure/LQR/nonlininear.png','-dpng','-r500')
 function d = diff_equ(t, X)
 global u Q R
 if t == 0
@@ -21,8 +26,14 @@ if t == 0
 end
 x = X(1:6);
 [A, B] = Quadcopter_system(x, u);
-[~, k1, ~] = icare(A, B, Q, R);
 x(1:3) = wrapToPi(x(1:3));
-d = (A - B * k1) * x;
-u = -k1 * x;
+try
+    [~, k1, ~] = icare(A, B, Q, R);
+    d = (A - B * k1) * x;
+    u = -k1 * x;
+catch
+    k1 = zeros(4, 6);
+    d = (A - B * k1) * x;
+    u = -k1 * x;
+end
 end
