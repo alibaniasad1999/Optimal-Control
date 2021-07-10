b891e67a27d6701bb855a879220b7e2bf42415af
Ali <alibaniasad1999@yahoo.com>
2021-07-10 17:44:18 +0430

    add new R

--- Project/Code/LQDG/LQDG_nonlinear_quad.m
+++ Project/Code/LQDG/LQDG_nonlinear_quad.m
@@ -1,8 +1,13 @@
 %%%%%%%%%%%%%%%%%%%%%% Quadcopter ODE45 %%%%%%%%%%%%%%%%%%%%%%
 QuadConstants;
-global R Q
-Q = 10 * eye(6);
-R = eye(4);
+global R_n Q u
+Q         = 10 * eye(6);
+u = ones(4, 1) * 2000;
+R         = eye(4);
+R_inv     = R^-1;
+Gamma     = 2 * eye(4);
+Gamma_inv = Gamma^-1;
+R_n       = (R_inv - Gamma_inv)^-1;
 x0	= [0 ;
     1 ;
     0 ;
@@ -10,7 +15,7 @@ x0	= [0 ;
     0 ;
     0];
 tic
-[t, x] = ode45(@diff_equ, [0, 7], x0);
+[t, x] = ode45(@diff_equ, [0, 3], x0);
 x(:, 1:3) = wrapToPi(x(:, 1:3));
 toc
 plot(t, x)
@@ -20,15 +25,13 @@ legend('$\phi$', '$\theta$', '$\psi$','$p$','$q$','$r$',...
     'interpreter', 'latex')
 print('../Figure/LQR/nonlininear.png','-dpng','-r500')
 function d = diff_equ(t, X)
-global u Q R
-if t == 0
-    u = ones(4, 1) * 2000;
-end
+global u Q R_n
 x = X(1:6);
 [A, B] = Quadcopter_system(x, u);
 x(1:3) = wrapToPi(x(1:3));
+t
 try
-    [~, k1, ~] = icare(A, B, Q, R);
+    [~, k1, ~] = icare(A, B, Q, R_n);
     d = (A - B * k1) * x;
     u = -k1 * x;
 catch
